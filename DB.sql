7ec8d12bf736907a59484232e1837bb74d666465
ahmed oueslati <72260223+ahmed-oues@users.noreply.github.com>
2023-11-23 17:51:08 +0100

    department fix 2

--- DB.sql
+++ DB.sql
@@ -116,7 +116,7 @@ CREATE TABLE Matieres (
 
 
 CREATE TABLE Departements (
-    Departement CHAR(55) NOT NULL,
+    Département CHAR(55) NOT NULL,
     Responsable CHAR(50) NULL,
     MatProf CHAR(10) NULL,
     DepartementARAB CHAR(55) NULL,
@@ -134,13 +134,13 @@ CREATE TABLE Jours (
     CodeProf SMALLINT NULL,
     CONSTRAINT PK_Jours PRIMARY KEY NONCLUSTERED (N)
 );
--- eroord
+ --Corrected SQL DB (Working with mysqli)   
 CREATE TABLE Options (
     `Option` CHAR(55) NOT NULL,
-    Departement CHAR(55) NULL,
-    OptionAraB CHAR(55) NULL,
-    CodeOption INT NULL,
-    PRIMARY KEY (`Option`)
+    `Département` CHAR(55) NOT NULL,
+    `OptionAraB` CHAR(55) NULL,
+    `CodeOption` INT NULL,
+    UNIQUE KEY `unique_option_departement` (`Option`, `Département`)
 );
 
 
