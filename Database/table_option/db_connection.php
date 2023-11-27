917c0c887385d9ef6fa6dd791ad1b5f60df202d7
Mohamed Amin Grami <mohamedgrami32@gmail.com>
2023-11-27 16:42:19 +0100

    Update db_connection.php

--- Database/table_option/db_connection.php
+++ Database/table_option/db_connection.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "user";
 $password = "user";
-$db_name = "scolarite"; // Update this line with your actual database name
+$db_name = "Scolarite"; // Update this line with your actual database name
 $conn = new mysqli($servername, $username, $password, $db_name);
 
 if ($conn->connect_error) {
