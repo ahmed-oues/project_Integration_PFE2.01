b769c8333880a54167f67b1021b9edb32dc79b58
ahmed oueslati <72260223+ahmed-oues@users.noreply.github.com>
2024-01-12 17:09:53 +0100

    table classe done

--- Database/table_Classe/db_con.php
+++ Database/table_Classe/db_con.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "gestionnaire_scolaire";
+$username = "user";
+$password = "user";
+$dbname = "Scolarite";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
