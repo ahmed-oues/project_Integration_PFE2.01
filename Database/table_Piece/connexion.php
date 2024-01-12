b7b9d09341ccf3d164aa3206e52808ca40ffd5eb
ahmed oueslati <72260223+ahmed-oues@users.noreply.github.com>
2024-01-12 17:00:14 +0100

    table Piece validere (changeed the sql file in the same  folder)

--- Database/table_Piece/connexion.php
+++ Database/table_Piece/connexion.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";  // Remplacez par votre nom de serveur MySQL
-$username = "root";  // Remplacez par votre nom d'utilisateur MySQL
-$password = "";  // Remplacez par votre mot de passe MySQL
-$dbname = "proj";  // Remplacez par le nom de votre base de données
+$username = "user";  // Remplacez par votre nom d'utilisateur MySQL
+$password = "user";  // Remplacez par votre mot de passe MySQL
+$dbname = "Scolarite";  // Remplacez par le nom de votre base de données
 
 // Créer la connexion
 $conn = new mysqli($servername, $username, $password, $dbname);
